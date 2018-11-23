3c2a49b86f013c059e0b299bbc394ce909e9db47
DevExpressExampleBot <fakeEmail>
2018-11-23 14:37:26 +0300

    Source auto update [skip ci]

---
+++ VB/SunburstHierarchicalDataAdapterSample/DataObjects.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Collections.Generic
+
+Namespace SunburstHierarchicalDataAdapterSample
+    Public Class CountryInfo
+        Public Property CountryName() As String
+        Public Property CityInfos() As New List(Of CityInfo)()
+    End Class
+    Public Class CityInfo
+        Public Property CityName() As String
+        Public Property SaleInfos() As New List(Of ProductInfo)()
+    End Class
+    Public Class ProductInfo
+        Public Property Category() As String
+        Public Property Total() As Double
+    End Class
+End Namespace
